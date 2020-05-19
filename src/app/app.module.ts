db33893b19ee2ff829b834df5f10afe321278cf8
mariusz.salwin <ml2QTDUeaM1m>
2020-05-19 22:01:02 +0200

    upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppComponent } from './app.component';
 
 // carbon-components-angular default imports
 import { UIShellModule } from 'carbon-components-angular';
-import { Notification20Module } from '@carbon/icons-angular/lib/notification/20';
-import { UserAvatar20Module } from '@carbon/icons-angular/lib/user--avatar/20';
-import { AppSwitcher20Module } from '@carbon/icons-angular/lib/app-switcher/20';
+import { NotificationModule } from '@carbon/icons-angular';
+import { UserAvatarModule } from '@carbon/icons-angular';
+import { AppSwitcherModule } from '@carbon/icons-angular';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
@@ -23,9 +23,9 @@ import { HeaderComponent } from './header/header.component';
 		FormsModule,
 		AppRoutingModule,
 		UIShellModule,
-		Notification20Module,
-		UserAvatar20Module,
-		AppSwitcher20Module
+		NotificationModule,
+		UserAvatarModule,
+		AppSwitcherModule
 	],
 	bootstrap: [AppComponent]
 })
